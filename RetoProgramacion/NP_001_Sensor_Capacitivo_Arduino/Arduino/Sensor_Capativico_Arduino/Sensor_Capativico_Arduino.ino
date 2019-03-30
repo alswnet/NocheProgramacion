2838a74eae1693b1ac091ea5b460d0229f3d1f4b
chepecarlos <chepecarlos@alswblog.org>
2019-03-30 01:50:26 -0600

    Codigo del primer video de ALSW :3

---
+++ RetoProgramacion/NP_001_Sensor_Capacitivo_Arduino/Arduino/Sensor_Capativico_Arduino/Sensor_Capativico_Arduino.ino
@@ -0,0 +1,39 @@
+#include <CapacitiveSensor.h>
+//Libreria se Sensor Capacitivo puede descargar
+//http://playground.arduino.cc/Main/CapacitiveSensor
+
+CapacitiveSensor   SensorC = CapacitiveSensor(4, 2);
+//Pines donde esta puesto el censor
+int led = 13;
+
+int limite = 100;
+//Limite que puede llegar el valor del sensor antes de encender el led
+//Cámbialo dependiendo de tu circuito, usa el puerto serial para ver que valor es el mas indicado
+
+
+void setup()
+{
+  SensorC.set_CS_AutocaL_Millis(0xFFFFFFFF);
+  //Inicializa el sensor y desactiva la autocorrecion
+
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  long Valor =  SensorC.capacitiveSensor(30);
+  //toma el dato del sensor en el tiempo establecido
+
+  Serial.println(Valor);
+  //Muestra el dato en el puerto serial para depuracion
+
+  if (Valor > limite) {
+    digitalWrite(led, HIGH);
+  }
+  else {
+    digitalWrite(led, LOW);
+  }
+
+  delay(100);
+}
---
+++ _RetoProgramacion/001_Sensor_Capacitivo_Arduino.md
@@ -0,0 +1,13 @@
+---
+title: "Sensor Capacitivo Arduino"
+video_number: 001
+date: 2014-10-24
+video_id: cjz_4ZptgqM
+repository: NP_001_Sensor_Capacitivo_Arduino
+
+links:
+  - title: "Libreria de Sensor Capacitivo"
+    url: "https://playground.arduino.cc/Main/CapacitiveSensor/"
+---
+
+Que puede hacer con 3 cables, 1 Resistencia, 1 Arduino y 1 Humano; un proyecto muy intersante
