1914a53ce30decbbc889c03282d4e6d5bc859325
chepecarlos <chepecarlos@alswblog.org>
2019-04-05 19:01:30 -0600

    corrigiendo pequeño error de functions

--- assets/javascript/functions.js
+++ assets/javascript/functions.js
@@ -69,25 +69,3 @@ window.addEventListener('load', () => {
     );
 });
 
-var allposts = [];
-
-function redirectToCorrectPage() {
-  { %
-    for post in site.posts %
-  }
-  allposts.push("{{ site.url }}{{ post.url }}"); { % endfor %
-  }
-  var url = window.location.toString();
-  if (url.slice(-1) === "/") {
-    url = url.slice(0, -1);
-  }
-  var allpostsUpperCase = allposts.map(function(value) {
-    return value.toUpperCase();
-  });
-  var i = allpostsUpperCase.indexOf(url.toUpperCase());
-  if (i != -1) {
-    console.log(allposts[i]);
-    window.location = allposts[i];
-  }
-}
-window.onload = redirectToCorrectPage;
