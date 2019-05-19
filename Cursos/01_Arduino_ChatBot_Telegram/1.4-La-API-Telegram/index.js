a558d74b9cd566764e6c32942d5c9f9b3f387ce9
chepecarlos <chepecarlos@alswblog.org>
2019-05-19 01:48:07 -0600

    Documentacion de curso 1.4

---
+++ Cursos/01_Arduino_ChatBot_Telegram/1.4-La-API-Telegram/index.js
@@ -0,0 +1,19 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = 'xxx';//Cambiar por el token de telegram
+const bot = new TelegramBot(token, {
+  polling: true
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  console.log("El ID del char" + chatId);
+  var Mensaje = msg.text;
+  if (Mensaje == "Encender") {
+    console.log("encendiendo el led");
+    bot.sendMessage(chatId, 'Encendere el led');
+  } else if (Mensaje == "Apagar") {
+    console.log("apagar el led");
+    bot.sendMessage(chatId, 'Apagare el led');
+  }
+});
