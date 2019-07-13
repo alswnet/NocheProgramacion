388a38e55d165e35363cf0b1efc0539e9ad34ead
chepecarlos <chepecarlos@alswblog.org>
2019-07-12 20:32:22 -0600

    Codigo del Usando LDR

---
+++ RetoProgramacion/058_Usando_LDR/Arduno/Usando_LDR/Usando_LDR.ino
@@ -0,0 +1,44 @@
+#include <EEPROM.h>
+
+int Led = 13;
+int LDR = A0;
+
+int Max, Min;
+
+#include <EEPROM.h>
+
+void setup() {
+  pinMode(Led, OUTPUT);
+  pinMode(LDR, INPUT);
+  Serial.begin(9600);
+  Max = EEPROM.read(1)*4;
+  Min = EEPROM.read(2)*4;
+}
+
+void loop() {
+  Serial.print(Max);
+  Serial.print(" - ");
+  Serial.print(Min);
+  Serial.print(" - ");
+  Serial.println(analogRead(LDR));
+
+  int ValorActual = analogRead(LDR);
+
+  if (ValorActual <= (Max + Min ) / 2) {
+    digitalWrite(Led, 1);
+  }
+  else {
+    digitalWrite(Led, 0);
+  }
+
+
+  if (Max < ValorActual) {
+    Max = ValorActual;
+    EEPROM.write(1, Max / 4);
+  }
+
+  if (Min > ValorActual) {
+    Min = ValorActual;
+    EEPROM.write(2, Min / 4);
+  }
+}
