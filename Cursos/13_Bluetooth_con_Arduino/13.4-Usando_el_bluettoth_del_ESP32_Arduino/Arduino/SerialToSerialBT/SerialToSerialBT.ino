b6ae9fd90fb687862ab4f520a5f28cc83d14afac
ChepeCarlos <chepecarlos@alswblog.org>
2020-08-10 12:52:17 -0600

    Suviendo Curso 13.4

--- Cursos/13_Bluetooth_con_Arduino/13.4-Usando_el_bluettoth_del_ESP32_Arduino/Arduino/SerialToSerialBT/SerialToSerialBT.ino
+++ Cursos/13_Bluetooth_con_Arduino/13.4-Usando_el_bluettoth_del_ESP32_Arduino/Arduino/SerialToSerialBT/SerialToSerialBT.ino
@@ -1,8 +1,3 @@
-//This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
-//
-//This example creates a bridge between Serial and Classical Bluetooth (SPP)
-//and also demonstrate that SerialBT have the same functionalities of a normal Serial
 
 #include "BluetoothSerial.h"
 
@@ -14,7 +9,7 @@ BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
+  SerialBT.begin("ESP32test");
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
