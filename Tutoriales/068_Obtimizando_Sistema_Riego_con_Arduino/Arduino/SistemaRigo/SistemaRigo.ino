55d841e629fc56a9ef1c5c756da5190c0bfeab78
ChepeCarlos <chepecarlos@alswblog.org>
2020-10-30 12:48:40 -0600

    Agregando tutorial 68

---
+++ Tutoriales/068_Obtimizando_Sistema_Riego_con_Arduino/Arduino/SistemaRigo/SistemaRigo.ino
@@ -0,0 +1,92 @@
+#include <Wire.h>
+#include "RTClib.h"
+int Led = 13;
+
+int HoraInicio = 6;
+int HoraFin = 18;
+int Periodo = 15;
+int PinRele = 2;
+int HoraAnteriorInicio = 0;
+int HoraAnteriorFin = 0;
+int PinBoton = 3;
+boolean Rele = false;
+
+RTC_DS1307 rtc;
+
+void setup() {
+  pinMode(PinBoton, INPUT);
+  pinMode(Led, OUTPUT);
+  digitalWrite(Led, 1);
+  pinMode(PinRele, OUTPUT);
+  digitalWrite(PinRele, 1);
+  Serial.begin (9600);
+  Wire.begin();
+  if (!rtc.begin()) {
+    Serial.println("RTC no funciona!");
+    ErrorLed();
+  }
+}
+
+void loop() {
+
+  DateTime ahora = rtc.now();
+  int Hora = ahora.hour();
+  int Minuto = ahora.minute();
+  // Serial.print(ahora.year(), DEC);
+  // Serial.print('/');
+  // Serial.print(ahora.month(), DEC);
+  // Serial.print('/');
+  // Serial.print(ahora.day(), DEC);
+  // Serial.print('/');
+  // Serial.print(ahora.hour(), DEC);
+  // Serial.print('/');
+  // Serial.print(ahora.minute(), DEC);
+  // Serial.print('/');
+  // Serial.print(ahora.second(), DEC);
+  // Serial.println();
+  // Serial.println();
+  // delay(1000);
+
+  FuncionRiego(Hora, Minuto);
+  FuncionBoton();
+
+  if (Rele) {
+    digitalWrite(PinRele, 1);
+  }
+  else {
+    digitalWrite(PinRele, 0);
+  }
+
+}
+
+void FuncionRiego(int Hora, int Minuto) {
+  if (Hora >= HoraInicio && Hora <= HoraFin ) {
+    if (Minuto <= Periodo && Hora != HoraAnteriorInicio) {
+      Serial.println("Encendiendo Rele");
+      Rele = true;
+      HoraAnteriorInicio = Hora;
+    }
+    else if (Minuto > Periodo && Hora != HoraAnteriorFin) {
+      Serial.println("Apagando Rele");
+      Rele = false;
+      HoraAnteriorFin = Hora;
+    }
+  }
+}
+
+void FuncionBoton() {
+  if (digitalRead(PinBoton) == 1) {
+    Serial.println("Cambiando estado de la Rele");
+    Rele = !Rele;
+    delay(300);
+  }
+}
+
+void ErrorLed() {
+  for (;;) {
+    digitalWrite(Led, 0);
+    delay(500);
+    digitalWrite(Led, 1);
+    delay(500);
+  }
+}
---
+++ _Tutoriales/068_Obtimizando_Sistema_Riego_con_Arduino.md
@@ -0,0 +1,18 @@
+---
+title: "Obtimizando Sistema Riego con Arduino"
+video_number: 68
+date: 2020-10-30
+video_id: CKkyL8lsFbg
+repository: 068_Obtimizando_Sistema_Riego_con_Arduino
+
+piezas:
+  - title: "Arduino UNO"
+  - title: "Modulo Rele"
+  - title: "Modulo RTC"
+  - title: "Led"
+  - title: "Resistencia 330ohm"
+  - title: "Boton"
+  - title: "Resistencia 10k"
+---
+
+Ayudamos a un amigo en Discord a mejorar su proyecto de Sistema de Riego con arduino, acompañanos a ver como optimizar su codigo
