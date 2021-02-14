492ac654536a6817934b39983e971989410c05a7
chepecarlos <chepecarlos@alswblog.org>
2021-02-13 22:24:20 -0600

    Mejorando codigo de 07.8 segun eslint

--- Cursos/07_Inteligencia_Artificial_ML5/07.8-Actualizando/P5/sketch.js
+++ Cursos/07_Inteligencia_Artificial_ML5/07.8-Actualizando/P5/sketch.js
@@ -10,45 +10,45 @@ function setup() {
   var ObtenerCanva = document.getElementById('micanva');
   var AnchoCanvas = ObtenerCanva.offsetWidth;
   CartaMensaje = document.getElementById('CartaMensaje');
-  CartaMensaje.innerText = "Cargando APP...";
+  CartaMensaje.innerText = 'Cargando APP...';
   Camara = createCapture(VIDEO);
   // Camara.size(1280, 720);
   Camara.hide();
   RelacionCamara = Camara.height / Camara.width;
   var AltoCanvas = AnchoCanvas * RelacionCamara;
   var sketchCanvas = createCanvas(AnchoCanvas, AltoCanvas);
-  sketchCanvas.parent("micanva");
+  sketchCanvas.parent('micanva');
 
   modelo = ml5.featureExtractor('MobileNet', ModeloListo);
   knn = ml5.KNNClassifier();
 
-  BotonesEntrenar = selectAll(".BotonEntrenar");
+  BotonesEntrenar = selectAll('.BotonEntrenar');
   for (var B = 0; B < BotonesEntrenar.length; B++) {
     BotonesEntrenar[B].mousePressed(PresionandoBoton);
   }
 
-  var TexBoxBoton = select("#TextBoxBoton");
+  var TexBoxBoton = select('#TextBoxBoton');
   TexBoxBoton.mousePressed(EntrenarTexBox);
 
-  var LimpiarBoton = select("#LimpiarBoton");
+  var LimpiarBoton = select('#LimpiarBoton');
   LimpiarBoton.mousePressed(LimpiarKnn);
 
-  var SalvarBoton = select("#SalvarBoton");
+  var SalvarBoton = select('#SalvarBoton');
   SalvarBoton.mousePressed(GuardadNeurona);
 
-  var CargarBoton = select("#CargarBoton");
+  var CargarBoton = select('#CargarBoton');
   CargarBoton.mousePressed(CargarNeurona);
 
   //CargarNeurona();
 }
 
 function draw() {
-  background("#b2dfdb");
+  background('#b2dfdb');
 
   image(Camara, 0, 0, width, height);
 
   if (knn.getNumLabels() > 0 && !Clasificando) {
-    console.log("Empezar a clasificar");
+    console.log('Empezar a clasificar');
     setInterval(clasificar, 500);
     Clasificando = true;
   }
@@ -58,7 +58,7 @@ function draw() {
     var Ancho = width;
     var Alto = Ancho * RelacionCamara2;
     RelacionCamara = RelacionCamara2;
-    console.log("Cambiando " + Ancho + " - " + Alto);
+    console.log('Cambiando ' + Ancho + ' - ' + Alto);
     resizeCanvas(Ancho, Alto, true);
   }
 }
@@ -71,13 +71,13 @@ function windowResized() {
 }
 
 function ModeloListo() {
-  console.log("Modelo Listo");
-  CartaMensaje.innerText = "Modelo Listo";
+  console.log('Modelo Listo');
+  CartaMensaje.innerText = 'Modelo Listo';
 }
 
 function PresionandoBoton() {
   var NombreBoton = this.elt.innerText;
-  console.log("Entrenando con " + NombreBoton);
+  console.log('Entrenando con ' + NombreBoton);
   EntrenarKnn(NombreBoton);
 }
 
@@ -91,7 +91,7 @@ function clasificar() {
     var Imagen = modelo.infer(Camara);
     knn.classify(Imagen, function(error, result) {
       if (error) {
-        console.log("Error en clasificar");
+        console.log('Error en clasificar');
         console.error();
       } else {
         console.log(result);
@@ -112,7 +112,7 @@ function clasificar() {
           Etiqueta = Etiquetas[Indice];
           Confianza = Math.ceil(Valores[Indice] * 100);
         }
-        CartaMensaje.innerText = Etiqueta + " - " + Confianza + "%";
+        CartaMensaje.innerText = Etiqueta + ' - ' + Confianza + '%';
       }
     });
   }
@@ -120,32 +120,32 @@ function clasificar() {
 
 function EntrenarTexBox() {
   var Imagen = modelo.infer(Camara);
-  var EtiquetaTextBox = select("#TextBox").value();
+  var EtiquetaTextBox = select('#TextBox').value();
   knn.addExample(Imagen, EtiquetaTextBox);
 }
 
 function LimpiarKnn() {
-  console.log("Borrando Neuroona");
+  console.log('Borrando Neuroona');
   if (Clasificando) {
     Clasificando = false;
     clearInterval(clasificar);
     knn.clearAllLabels();
-    CartaMensaje.innerText = "Neurona Borrada";
+    CartaMensaje.innerText = 'Neurona Borrada';
   }
 }
 
 function GuardadNeurona() {
   if (Clasificando) {
-    console.log("Guardando la neurona");
-    knn.save("NeuronaKNN");
+    console.log('Guardando la neurona');
+    knn.save('NeuronaKNN');
   }
 }
 
 function CargarNeurona() {
-  console.log("Cargando una Neurona");
-  knn.load("./data/NeuronaKNN.json", function() {
-    console.log("Neurona Cargada knn");
-    CartaMensaje.innerText = "Neurona cargana de archivo";
+  console.log('Cargando una Neurona');
+  knn.load('./data/NeuronaKNN.json', function() {
+    console.log('Neurona Cargada knn');
+    CartaMensaje.innerText = 'Neurona cargana de archivo';
     CargandoNeurona = true;
   });
 }
