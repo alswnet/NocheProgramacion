ac6d6585f4a7b0f36786cad3b0903f39f89369f9
chepecarlos <chepecarlos@alswblog.org>
2021-03-14 14:03:14 -0600

    Limpiando codigo de tutorial 079

--- Tutoriales/079_MQTT_en_Javascript/p5js/sketch.js
+++ Tutoriales/079_MQTT_en_Javascript/p5js/sketch.js
@@ -1,9 +1,9 @@
-var client = mqtt.connect('ws://test.mosquitto.org:8080/mqtt')
+var client = mqtt.connect("ws://test.mosquitto.org:8080/mqtt")
 
 function EventoConectar() {
-  client.subscribe('ALSW/#', function(err) {
+  client.subscribe("ALSW/#", function(err) {
     if (!err) {
-      client.publish('ALSW/Temperatura', '30')
+      client.publish("ALSW/Temperatura", "30");
     }
   })
 }
@@ -12,9 +12,9 @@ function EventoMensaje(topic, message) {
   if (topic == "ALSW/temp") {
     console.log("La Temperatura es " + message.toString());
   }
-  console.log(topic + " - " + message.toString())
+  console.log(topic + " - " + message.toString());
   // client.end()
 }
 
-client.on('connect', EventoConectar)
-client.on('message', EventoMensaje)
+client.on("connect", EventoConectar);
+client.on("message", EventoMensaje);
