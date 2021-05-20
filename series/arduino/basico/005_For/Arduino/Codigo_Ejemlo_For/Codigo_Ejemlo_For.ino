07b8e7aa06602b239d1a2b9be69ba3d8ec2eeddb
chepecarlos <chepecarlos@alswblog.org>
2021-05-20 17:16:27 -0600

    Codigo Arduino de Comando 005 de for

---
+++ series/arduino/basico/005_For/Arduino/Codigo_Ejemlo_For/Codigo_Ejemlo_For.ino
@@ -0,0 +1,10 @@
+void setup() {
+  Serial.begin(9600);
+  for (int i = 10 ; i >= 0; i--) {
+    Serial.println(i);
+  }
+}
+
+void loop() {
+  
+}
---
+++ series/arduino/basico/005_For/Arduino/Codigo_Final/Codigo_Final.ino
@@ -0,0 +1,23 @@
+int CanitdadLed = 10;
+int LedInicial = 3;
+int Esperar = 100;
+
+void setup() {
+  for (int i = LedInicial ; i < CanitdadLed + LedInicial ; i++) {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = LedInicial ; i < CanitdadLed + LedInicial ; i++) {
+    digitalWrite(i , HIGH);
+    delay(Esperar);
+    digitalWrite(i, LOW);
+  }
+
+  for (int i = CanitdadLed + LedInicial - 1  ; i >= LedInicial ; i--) {
+    digitalWrite(i , HIGH);
+    delay(Esperar);
+    digitalWrite(i, LOW);
+  }
+}
