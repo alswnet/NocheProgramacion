93257825a77915db954af671adb5b85b4b53fe64
chepecarlos <chepecarlos@alswblog.org>
2021-07-05 23:06:01 -0600

    Mejorando codigo del proyecto del braso con funciones para salvar y cargaar data

--- series/braso_arduino/CodigoDemo/CodigoDemo.ino
+++ series/braso_arduino/CodigoDemo/CodigoDemo.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include <EEPROM.h>
 
 struct SuperServo {
   unsigned int Pin;
@@ -9,25 +10,32 @@ struct SuperServo {
   unsigned int PosicionMinima;
   unsigned int PulsoMaximo;
   unsigned int PulsoMinimo;
+  unsigned int EEPRON_Descanso;
   Servo Motor;
 };
 
 int CantidadMotores = 6;
 SuperServo Motores[6] = {
-  {2, 0, 0, 90, 170, 10, 2400, 544},//Motor 0
-  {3, 0, 0, 90, 170, 10, 2400, 544},
-  {4, 0, 0, 90, 170, 10, 2400, 544},
-  {5, 0, 0, 90, 170, 10, 2400, 544},
-  {6, 0, 0, 90, 170, 10, 2400, 544},
-  {7, 0, 0, 90, 170, 10, 2400, 544}
+  {2, 0, 0, 0, 170, 10, 2400, 544, 1},//Motor 0
+  {3, 0, 0, 0, 170, 10, 2400, 544, 2},
+  {4, 0, 0, 0, 170, 10, 2400, 544, 3},
+  {5, 0, 0, 0, 170, 10, 2400, 544, 4},
+  {6, 0, 0, 0, 170, 10, 2400, 544, 5},
+  {7, 0, 0, 0, 170, 10, 2400, 544, 6}
 };
+
 float Esperar = 100;
+int DirecionEspera = 0;
 float TiempoAnterior = 0;
 
 void setup() {
   Serial.begin(9600);
+
+  Esperar = 10 * EEPROM.read(DirecionEspera);
+
   for (int i = 0; i < CantidadMotores; i++) {
     Motores[i].Motor.attach(Motores[i].Pin, Motores[i].PulsoMinimo, Motores[i].PulsoMaximo);
+    Motores[i].PosicionDescanso = EEPROM.read(Motores[i].EEPRON_Descanso);
     Motores[i].PosicionDeseada = Motores[i].PosicionDescanso;
     Motores[i].PosicionActual = Motores[i].PosicionDescanso;
     Motores[i].Motor.write(Motores[i].PosicionActual);
@@ -52,30 +60,34 @@ void DecodificarSerial() {
 
   String Mensaje = Serial.readStringUntil('\n');
 
-  //  Serial.print("Mensaje : ");
-  //  Serial.println(Mensaje);
-
-  if (Mensaje.equals("d")) {
-    PosicionDescanso();
-  }
+  boolean Salir = MensajeConfiguracion(Mensaje);
+  if (Salir) return;
 
   int PosicionPleca = Mensaje.indexOf('/');
   int PosicionSaltoLinea = Mensaje.length();
 
   if (PosicionPleca < 0) return;
 
-  int IDMotor = Mensaje.substring(0, PosicionPleca).toInt();
-  int Posicion = Mensaje.substring(PosicionPleca + 1, PosicionSaltoLinea).toInt();
-  Posicion = constrain(Posicion, 0, 180);
+  String Tipo = Mensaje.substring(0, PosicionPleca);
+  int Valor = Mensaje.substring(PosicionPleca + 1, PosicionSaltoLinea).toInt();
+
+  if (Tipo.equals("v")) {
+    Esperar = Valor * 10;
+    EEPROM.write(DirecionEspera, Valor);
+    Serial.print("v/");
+    Serial.println(Valor);
+    return;
+  }
+
+
+  int IDMotor = Tipo.toInt();
+
+  int Posicion = constrain(Valor, 0, 180);
 
   if (IDMotor >= CantidadMotores) return;
 
   Motores[IDMotor].PosicionDeseada = constrain(Posicion, Motores[IDMotor].PosicionMinima, Motores[IDMotor].PosicionMaxima);
 
-  //  Serial.print("Motor : ");
-  //  Serial.print(IDMotor);
-  //  Serial.print(" Grados : ");
-  //  Serial.println(Posicion);
 }
 
 void ActualizarServos() {
@@ -91,14 +103,6 @@ void ActualizarServos() {
           Motores[i].PosicionActual++;
         }
 
-        //        Serial.print("Moviendo el servo [");
-        //        Serial.print(i);
-        //        Serial.print("] ");
-        //        Serial.print(Motores[i].PosicionActual);
-        //        Serial.print(" a ");
-        //        Serial.print(Motores[i].PosicionDeseada);
-        //        Serial.println();
-
         Serial.print(i);
         Serial.print("/");
         Serial.println(Motores[i].PosicionActual);
@@ -109,8 +113,31 @@ void ActualizarServos() {
   }
 }
 
-void PosicionDescanso() {
+void ReutinaDescanso() {
   for (int i = 0; i < CantidadMotores; i++) {
     Motores[i].PosicionDeseada = Motores[i].PosicionDescanso;
   }
+}
+
+boolean MensajeConfiguracion(String Mensaje) {
+  if (Mensaje.equals("d")) {
+    ReutinaDescanso();
+  } else if (Mensaje.equals("v")) {
+    Serial.print("v/");
+    Serial.println(Esperar);
+  } else if (Mensaje.equals("e")) {
+    for (int i = 0; i < CantidadMotores; i++) {
+      Serial.print(i);
+      Serial.print("/");
+      Serial.println( Motores[i].PosicionActual);
+    }
+  } else if (Mensaje.equals("s")) {
+    for (int i = 0; i < CantidadMotores; i++) {
+      Motores[i].PosicionDescanso = Motores[i].PosicionActual;d
+      EEPROM.write(Motores[i].EEPRON_Descanso, Motores[i].PosicionDescanso);
+    }
+  }  else {
+    return false;
+  }
+  return true;
 }
