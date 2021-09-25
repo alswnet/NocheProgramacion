81e4cecdb86cdac3620123bc857ed6dffc818cf1
chepecarlos <chepecarlos@alswblog.org>
2021-09-24 23:34:19 -0600

    Agregando "Facil" de web server para el esp32 y esp8266

---
+++ series/arduino_esp/007_Web_Server_Facil/Arduino/Web_Server_Facil/data.h
@@ -0,0 +1,12 @@
+
+const char* ssid_1     = "xxxx";
+const char* password_1 = "xxxx";
+const char* ssid_2     = "Subcribanse";
+const char* password_2 = "alswnet";
+
+const String Pagina =  R"====(HTTP/1.1 200 OK
+Content-Type: text/html
+
+<!DOCTYPE HTML>
+<html>
+)====";
