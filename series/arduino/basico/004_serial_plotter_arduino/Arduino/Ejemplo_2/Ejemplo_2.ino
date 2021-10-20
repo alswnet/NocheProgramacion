0c29d58b6852fc5df47c20c3acb15354a9ecd2c9
chepecarlos <chepecarlos@alswblog.org>
2021-10-19 18:26:22 -0600

    Agregando codigo de Serial ploter

---
+++ series/arduino/basico/004_serial_plotter_arduino/Arduino/Ejemplo_2/Ejemplo_2.ino
@@ -0,0 +1,31 @@
+// Creado ChepeCarlos de ALSW
+// Tutorial Completo en https://nocheprogramacion.com
+// Canal Youtube https://youtube.com/alswnet?sub_confirmation=1
+
+int Boton1 = 2;
+int Boton2 = 3;
+int Boton3 = 4;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Boton1, INPUT);
+  pinMode(Boton2, INPUT);
+  pinMode(Boton3, INPUT);
+}
+
+void loop() {
+  int Estado1 = digitalRead(Boton1) * 3;
+  int Estado2 = digitalRead(Boton2) * 4;
+  int Estado3 = digitalRead(Boton3) * 5;
+
+  Serial.print("Izquierda:");
+  Serial.print(Estado1);
+  Serial.print("\t");
+  Serial.print("Centro:");
+  Serial.print(Estado2);
+  Serial.print("\t");
+  Serial.print("Derecha:");
+  Serial.print(Estado3);
+  Serial.println();
+  delay(100);
+}
---
+++ series/arduino/basico/004_serial_plotter_arduino/Arduino/Ejemplo_3/Ejemplo_3.ino
@@ -0,0 +1,25 @@
+// Creado ChepeCarlos de ALSW
+// Tutorial Completo en https://nocheprogramacion.com
+// Canal Youtube https://youtube.com/alswnet?sub_confirmation=1
+
+int Boton = 2;
+float PosicionActual = 0;
+float PosicionDeseada = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Boton, INPUT);
+}
+
+void loop() {
+  int PosicionDeseada = digitalRead(Boton) * 180;
+  PosicionActual = (0.05 * PosicionDeseada) + (0.95 * PosicionActual);
+
+  Serial.print("Actual:");
+  Serial.print(PosicionActual);
+  Serial.print("\t");
+  Serial.print("Deseada:");
+  Serial.print(PosicionDeseada);
+  Serial.println();
+  delay(100);
+}
