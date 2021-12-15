a20504cac859c1885effd1d805a06322fe60b914
chepecarlos <chepecarlos@alswblog.org>
2021-12-15 13:50:01 -0600

    Codigo Base

--- series/simon_dice/Arduino/Simon_Dice/Simon_Dice.ino
+++ series/simon_dice/Arduino/Simon_Dice/Simon_Dice.ino
@@ -1,7 +1,11 @@
 
 #define Iniciando 0
-#define Esperando 1
-#define pollo 2
+#define Animacion 1
+#define Esperando 2
+#define Calcular 3
+#define SiquienteRonda 3
+#define Perder 4
+#define Ganar 5
 
 int Led[] = {2, 4, 6, 8};
 int Boton[] = {3, 5, 7, 9};
@@ -11,9 +15,17 @@ int Cantidad = 4;
 int Buzzer = 10;
 int Estado = Iniciando;
 
-#define RondaGanar 10
-int Ronda = 0;
+int PinVelocidad = A0;
+int VelocidadMinima = 100;
+int VelocidadMaxima = 2000;
+
+int Espera = 400;
+
+#define RondaGanar 3
 int Secuencias[RondaGanar];
+int Ronda = 0;
+int I_Ronda = 0;
+int I_Animacion = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -22,55 +34,68 @@ void setup() {
     pinMode(Led[i], OUTPUT);
   }
   pinMode(Buzzer, OUTPUT);
+  pinMode(PinVelocidad, INPUT);
 }
 
 void loop() {
-
+  int Boton_ID = LeerBotones();
+  int Espera = LeerVelocidad();
   switch (Estado) {
     case Iniciando:
-      GenerarSecuencia();
+      GenerarSecuencia(Espera);
       Ronda = 0;
-      Estado = Esperando;
+      I_Ronda = 0;
+      Estado = Animacion;
+      I_Animacion = 0;
+      delay(300);
+      break;
+    case Animacion:
+      {
+        delay(Espera / 2);
+        EncenderLed(Secuencias[I_Animacion]);
+        delay(Espera);
+        ApagarLed(Secuencias[I_Animacion]);
+        Serial.print("R");
+        Serial.print(I_Animacion);
+        Serial.print(" - ");
+        Serial.println(Secuencias[I_Animacion]);
+        I_Animacion++;
+        if ( I_Animacion > Ronda) {
+          Estado = Esperando;
+          I_Animacion = 0;
+        }
+      }
       break;
     case Esperando:
-      int Boton = LeerBotones();
-      Serial.print("B:");
-      Serial.println(Boton);
-
-      if (Boton < 0) {
-        return;
-      }
-      Serial.println(Boton);
-      EncenderLed(Boton);
-      delay(200);
-      while (Boton == LeerBotones()) {
-        Serial.print(".");
-        delay(50);
-      }
-      ApagarLed(Boton);
-      Estado = pollo;
-      Serial.print(Estado);
-      Serial.println(" a calcular");
+      Estado_Esperando(Boton_ID, Espera);
       break;
-    case pollo:
+    case SiquienteRonda:
       Ronda++;
-      Serial.print("R:");
-      Serial.println(Ronda);
+      I_Ronda = 0;
+      Serial.print("Ronda:");
+      Serial.println(Ronda + 1);
+
       if (Ronda >= RondaGanar) {
-        Serial.println("Ganaste :D");
-        Estado = Iniciando;
+
+        Estado = Ganar;
         return;
       }
-      Estado = Esperando;
+      Estado = Animacion;
+      break;
+    case Perder:
+      Serial.print("Perdio en ");
+      Serial.println(Ronda);
+      SecuenciaPerder(Espera);
+      SecuenciaPerder(Espera);
+      Estado = Iniciando;
+      break;
+    case Ganar:
+      Serial.println("Ganaste :D");
+      Estado = Iniciando;
       break;
     default:
       Estado = Iniciando;
       break;
-  }
-
-  Serial.println(Estado);
-  if (Estado == pollo) {
-    Serial.println("Cancular");
   }
 
 }
@@ -85,6 +110,13 @@ int LeerBotones() {
   return -1;
 }
 
+int LeerVelocidad() {
+  int Valor = analogRead(PinVelocidad);
+  Valor = map(Valor, 0, 1023, VelocidadMinima, VelocidadMaxima);
+  Valor = constrain(Valor, VelocidadMinima, VelocidadMaxima);
+  return Valor;
+}
+
 void EncenderLed(int ID_Led) {
   digitalWrite(Led[ID_Led], HIGH);
   tone(Buzzer, Tonos[ID_Led]);
@@ -95,34 +127,70 @@ void ApagarLed(int ID_Led) {
   noTone(Buzzer);
 }
 
-void GenerarSecuencia() {
+void GenerarSecuencia(int Velocidad) {
   randomSeed(analogRead(A1));
 
   for (int i = 0; i < RondaGanar; i++) {
     Secuencias[i] = round(random(0, Cantidad));
-    //    Serial.print(i);
-    //    Serial.print(" ");
-    //    Serial.println(Secuencias[i]);
   }
 
-  SecuenciaLed();
-  //  SecuenciaLed();
+  SecuenciaLed(Velocidad);
 }
 
-void SecuenciaLed() {
+void SecuenciaLed(int Velocidad) {
   for (int i = 0; i < Cantidad; i++) {
-    tone(Buzzer, Tonos[i], 200);
-    digitalWrite(Led[i], HIGH);
-    delay(200);
-    digitalWrite(Led[i], LOW);
+    EncenderLed(i);
+    delay(Velocidad);
+    ApagarLed(i);
     noTone(Buzzer);
   }
 
   for (int i = Cantidad - 1; i >= 0; i--) {
-    tone(Buzzer, Tonos[i], 200);
-    digitalWrite(Led[i], HIGH);
-    delay(200);
-    digitalWrite(Led[i], LOW);
+    EncenderLed(i);
+    delay(Velocidad);
+    ApagarLed(i);
     noTone(Buzzer);
+  }
+}
+
+void SecuenciaPerder(int Velocidad) {
+  for (int i = 0; i < Cantidad; i++) {
+    EncenderLed(i);
+  }
+  delay(Velocidad);
+  for (int i = 0; i < Cantidad; i++) {
+    ApagarLed(i);
+    noTone(Buzzer);
+  }
+}
+
+void Estado_Esperando(int Boton_ID, int Espera) {
+  if (Boton_ID < 0) return;
+  EncenderLed(Boton_ID);
+  delay(Espera);
+  while (Boton_ID == LeerBotones()) {
+    Serial.print(".");
+    delay(25);
+  }
+  ApagarLed(Boton_ID);
+  Serial.print("I:");
+  Serial.print(I_Ronda);
+  Serial.print(" S:");
+  Serial.print(Secuencias[I_Ronda]);
+  Serial.print(" B:");
+  Serial.print(Boton_ID);
+  Serial.print(" R:");
+  Serial.println(Ronda);
+  if ( Secuencias[I_Ronda] == Boton_ID ) {
+    if (I_Ronda >= Ronda) {
+      Estado = SiquienteRonda;
+    }
+    else {
+      Estado = Esperando;
+      I_Ronda++;
+    }
+
+  } else {
+    Estado = Perder;
   }
 }
