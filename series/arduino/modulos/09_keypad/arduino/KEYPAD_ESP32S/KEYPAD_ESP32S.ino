df4824d0e3a26e06bfe280a7ff0c3c731c394675
parada15 <luisparada0085@gmail.com>
2024-09-07 14:51:09 -0600

    Agregando diagrma y codigo de keypad esp 32 y 8266

---
+++ series/arduino/modulos/09_keypad/arduino/KEYPAD_ESP32S/KEYPAD_ESP32S.ino
@@ -0,0 +1,33 @@
+// importa libreria Keypad
+#include <Keypad.h>
+// define numero de filas
+const uint8_t ROWS = 4;
+// define numero de columnas
+const uint8_t COLS = 4;
+// define la distribucion de teclas
+char keys[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
+// pines correspondientes a las filas
+uint8_t rowPins[ROWS] = { 16, 4, 2, 15 };
+// pines correspondientes a las columnas
+uint8_t colPins[COLS] = { 19, 18, 5, 17 };
+// crea objeto con los prametros creados previamente
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  // obtiene tecla presionada y asigna a variable
+  char key = keypad.getKey();
+  // comprueba que se haya presionado una tecla
+  if (key) {
+    // envia a monitor serial la tecla presionada
+    Serial.println(key);
+  }
+}
